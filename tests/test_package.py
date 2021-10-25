afa00184a0d50097e3c0f43314915b7a11640b52
Lindsey Gray <lindsey.gray@gmail.com>
2021-10-25 13:25:28 -0500

    import all relevant subpackages

--- tests/test_package.py
+++ tests/test_package.py
@@ -3,3 +3,14 @@ import hgg_coffea as m
 
 def test_version():
     assert m.__version__
+
+
+def test_tools_subpackage():
+    from hgg_coffea.tools import chained_quantile
+    from hgg_coffea.tools import diphoton_mva
+    from hgg_coffea.tools import xgb_loader
+
+
+def test_workflows_subpackage():
+    from hgg_coffea.workflows import dystudies
+    from hgg_coffea.workflows import base
